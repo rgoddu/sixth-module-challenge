3b632dbae55bebcaafc229a3ecfd9ea03802cec7
Remi G <rgoddu909@gmail.com>
2023-10-30 12:46:37 -0400

    weather

--- javascript.js
+++ javascript.js
@@ -14,20 +14,20 @@ searchButton.click(function(){
             return response.json()
         })
         .then(function (data) {
-            console.log( data)
             var latitude = data[0].lat
             var longitude = data[0].lon
             var coords = "lat=" + latitude + "&lon=" + longitude
-
+            
             var currentWeatherURL = "http://api.openweathermap.org/data/2.5/weather?" + coords + "&units=imperial&appid=" + weatherAPIKey
             var forecastURL = "http://api.openweathermap.org/data/2.5/forecast?" + coords + "&cnt&units=imperial&appid=" + weatherAPIKey
             
             function getWeather() {
                 fetch(currentWeatherURL)
-                    .then(function (response) {
-                        return response.json()
-                    })
-                    .then(function (data) {
+                .then(function (response) {
+                    return response.json()
+                })
+                .then(function (data) {
+                        console.log(data)
                         // var temperature = ((data.main.temp - 273.15) * 1.8) + 32
                         var temperature = data.main.temp
                         console.log("current weather \n ---------------")
@@ -36,11 +36,14 @@ searchButton.click(function(){
                         console.log(data.weather[0].description)
                         console.log("humidity " + data.main.humidity + "\n =======")
                         var currentTemp = $("#current-temp")
-                        currentTemp.text("Temperature: " + temperature)
+                        currentTemp.text("Temperature: " + temperature + " °F")
                         var currentHumidity = $("#current-humidity")
-                        currentHumidity.text("Humidity: " + data.main.humidity)
+                        currentHumidity.text("Humidity: " + data.main.humidity + "%")
                         var currentWind = $("#current-wind")
-                        currentWind.text("Wind speed: " + data.wind.speed)
+                        currentWind.text("Wind speed: " + data.wind.speed+" mph")
+                        iconNumber = data.weather[0].icon
+                        var iconSpan = $("#icon-span")
+                        iconSpan.attr("src", "https://openweathermap.org/img/wn/"+iconNumber+"@2x.png")
                     })
             }
             getWeather()
@@ -63,11 +66,17 @@ searchButton.click(function(){
                             var forecastTime = dayjs(tempTimeDate).format("H")
                             // console.log(forecastTime)
                             if(forecastTime==temperatureTimeTaken){
-                                var weekday = dayjs(tempTimeDate).format("dddd")
-                                var dayInfo = Math.floor(i/8)
-                                // console.log("number:" + dayInfo)
-                                var weekdayContainer = $("#forecast-"+dayInfo)
+                                var weekday = dayjs(tempTimeDate).format("ddd")
+                                var dayNumber = Math.floor(i/8)
+                                // console.log("number:" + dayNumber)
+                                var weekdayContainer = $("#forecast-"+dayNumber)
                                 weekdayContainer.text(weekday)
+                                var tempContainer = $("#t" +dayNumber)
+                                tempContainer.text(data.list[i].main.temp + " °F")
+                                var hContainer = $("#h"+dayNumber)
+                                hContainer.text(data.list[i].main.humidity + "% humidity")
+                                var wContainer = $("#w"+dayNumber)
+                                wContainer.text(data.list[i].wind.speed + " mph windspeed")
                                 console.log(weekday)
                                 console.log(data.list[i].main.temp)
                             }
